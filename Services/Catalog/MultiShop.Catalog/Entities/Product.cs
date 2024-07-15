7051345cd3655f7a038d3583e7a41606c6cab2d8
Elen <elen-kocak@hotmail.com>
2024-07-15 19:59:50 +0300

    Add project files.

---
+++ Services/Catalog/MultiShop.Catalog/Entities/Product.cs
@@ -0,0 +1,32 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MultiShop.Catalog.Entities
+{
+    public class Product
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string ProductId { get; set; }
+
+        public string ProductName { get; set; }
+
+        public decimal ProductPrice { get; set; }
+
+        public string ProductImageUrl { get; set; }
+
+        public string ProductDescription { get; set; }
+
+        public string CategoryId { get; set; }
+
+        [BsonIgnore]
+        public Category Category { get; set; }
+    }
+    #region BsonIgnore Nedir
+    /*
+      BsonIgnore attribute'ü MongoDB koleksiyonlarında 
+    saklanmasını istemediğiniz veya gereksiz bulduğunuz 
+    class property'lerini işaretlemek için kullanılır.
+    */
+    #endregion
+}
